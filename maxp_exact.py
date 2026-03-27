7ced0f56a208417f1bf1150c60fa1187b7ef3b6b
hegen008 <hegen008@umn.edu>
2026-03-27 18:29:21 -0500

    initial class and functions

---
+++ maxp_exact.py
@@ -0,0 +1,20 @@
+from pulp import *
+import highspy
+
+class MaxPHeuristic():
+    # vector initialization
+    def __init__():
+        pass
+
+    # geopandas initialization
+    def __init__():
+        pass
+
+    # construct MIP model
+    def construct():
+        pass
+
+    # solve MIP model
+    def solve():
+        pass
+    
