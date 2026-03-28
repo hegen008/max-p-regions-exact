f31f4d6a865115b2c4263ccff33d0fe5bd35efb2
hegen008 <hegen008@umn.edu>
2026-03-28 14:03:27 -0500

    Fix base optimization and add methods list

--- maxp_exact.py
+++ maxp_exact.py
@@ -2,6 +2,20 @@ from pulp import *
 import highspy
 import numpy as np
 from libpysal import weights
+from dataclasses import dataclass, field
+
+@dataclass
+class MaxPConfig:
+    bound_num_regions: bool = False
+    bound_contiguity: bool = False
+    merge_leaves: bool = False
+    independent_split: bool = False
+    reduce_size_of_t: bool = False
+    preassign_roots: bool = False
+    exclude_roots: bool = False
+    max_attr_for_root: bool = False
+    min_adj_order: bool = False
+    sort_regions: bool = False
 
 class MaxPExact():
     # array initialization
@@ -17,7 +31,8 @@ class MaxPExact():
         self.spatial_weights = weights.full2W(adj_mat)
         self.regions = np.empty(self.num_areas, dtype=object)
         self.maxp = None
-        self.weight_factor = 1 + np.floor(np.log10(np.sum(np.triu(sim_mat, k=1))))
+        self.obj = None
+        self.weight_factor = 10**(1 + np.floor(np.log10(np.sum(np.triu(sim_mat, k=1)))))
 
     # geopandas initialization
     @classmethod
@@ -35,31 +50,34 @@ class MaxPExact():
         self.C_set = range(self.num_areas)
 
         # Decision variables
-        self.x = LpVariable.dicts("x", (self.I_set, self.K_set, self.C_set), cat="Binary")
-        self.t = LpVariable.dicts("t", (self.I_set, self.I_set), cat="Binary")
+        self.x = LpVariable.dicts("var_x", (self.I_set, self.K_set, self.C_set), cat="Binary")
+        self.t = LpVariable.dicts("var_t", [(i, j) for i in self.I_set for j in self.I_set if j > i], cat="Binary")
 
         # Objective function
-        self.model += lpSum(self.x[i][j][0] for i in self.I_set for j in self.K_set), "Objective"
+        self.model += lpSum(self.x[i][j][0] for i in self.I_set for j in self.K_set) * self.weight_factor + lpSum(self.t[i,j] * self.sim_mat[i][j] for i in self.I_set for j in self.I_set if j > i), "Objective"
 
         # Define base model constraints
         for k in self.K_set:
-            self.model += lpSum(self.x[i][k][0] for i in self.I_set) <= 1, f"Single_Root_{k}"
-            self.model += lpSum(self.x[i][k][c] * self.spatial_attr[i] for i in self.I_set for c in self.C_set) >= self.threshold * lpSum(self.x[i][k][0] for i in self.I_set), f"Threshold_{k}"
+            self.model += lpSum(self.x[i][k][0] for i in self.I_set) <= 1 #, f"Single_Root_{k}"
+            self.model += lpSum(self.x[i][k][c] * self.spatial_attr[i] for i in self.I_set for c in self.C_set) >= self.threshold * lpSum(self.x[i][k][0] for i in self.I_set) #, f"Threshold_{k}"
         for i in self.I_set:
-            self.model += lpSum(self.x[i][k][c] for k in self.K_set for c in self.C_set) == 1, f"Single_Assignment_{i}"
+            self.model += lpSum(self.x[i][k][c] for k in self.K_set for c in self.C_set) == 1 #, f"Single_Assignment_{i}"
             for k in self.K_set:
                 for c in self.C_set:
                     if c > 0:
-                        self.model += self.x[i][k][c] <= lpSum(self.x[j][k][c-1] for j in self.spatial_weights.neighbors[i]), f"Adjacency_{i}_{k}_{c}"
+                        self.model += self.x[i][k][c] <= lpSum(self.x[j][k][c-1] for j in self.spatial_weights.neighbors[i]) #, f"Adjacency_{i}_{k}_{c}"
                 for j in self.I_set:
-                    if j < i:
-                        self.t[i][j] <= lpSum(self.x[i][k][c] - self.x[j][k][c] for c in self.C_set) + 1, f"x_t_Matching_{i}_{j}_{k}"
+                    if j > i:
+                        self.model += self.t[i,j] <= lpSum(self.x[i][k][c] - self.x[j][k][c] for c in self.C_set) + 1 #, f"x_t_Matching_{i}_{j}_{k}"
 
     # solve MIP model
     def solve(self, time):
-        self.solver = getSolver("PULP_CBC_CMD", timeLimit=time)
-        self.model.solve(self.solver)
+        self.model.solve(HiGHS(timeLimit=time, keepFiles=False))
         self.maxp = int(value(lpSum(self.x[i][k][0] for i in self.I_set for k in self.K_set)))
+        self.obj = value(self.model.objective)
+
+        print({f"{i}_{j}":value(self.t[i,j]) for i in self.I_set for j in self.I_set if j > i})
+
         assigned = {(i,k) for i in self.I_set for k in self.K_set for c in self.C_set if value(self.x[i][k][c]) > 0.9}
         for i,k in assigned:
             self.regions[i] = k
