a0cc0bee7e82008dfc68d8c77ea903997b58213b
hegen008 <hegen008@umn.edu>
2026-03-29 14:19:51 -0500

    add bound m method

--- maxp_exact.py
+++ maxp_exact.py
@@ -4,6 +4,12 @@ import numpy as np
 from libpysal import weights
 from dataclasses import dataclass, field
 
+def _bound_num_regions(spatial_attr, threshold):
+    over_thres = np.sum(spatial_attr > threshold)
+    under_thres = np.sum(spatial_attr[spatial_attr < threshold])
+    under_thres //= threshold
+    return over_thres + under_thres
+
 @dataclass
 class MaxPConfig:
     bound_num_regions: bool = False
@@ -40,55 +46,58 @@ class MaxPExact():
         pass
 
     # construct MIP model
-    def construct(self):
+    def construct(self, config):
         # Initialize model
         self.model = LpProblem("MaxP_Model", LpMaximize)
 
         # Define iterative ranges
-        self.I_set = range(self.num_areas)
-        self.K_set = range(self.num_areas)
-        self.C_set = range(self.num_areas)
+        self._I_set = range(self.num_areas)
+        if config.bound_num_regions:
+            self._K_set = range(_bound_num_regions(self.spatial_attr, self.threshold))
+        else:
+            self._K_set = range(self.num_areas)
+        self._C_set = range(self.num_areas)
 
         # Decision variables
-        self.x = LpVariable.dicts("var_x", (self.I_set, self.K_set, self.C_set), cat="Binary")
-        self.t = LpVariable.dicts("var_t", [(i, j) for i in self.I_set for j in self.I_set if j > i], cat="Binary")
+        self.x = LpVariable.dicts("var_x", (self._I_set, self._K_set, self._C_set), cat="Binary")
+        self.t = LpVariable.dicts("var_t", [(i, j) for i in self._I_set for j in self._I_set if j > i], cat="Binary")
 
         # Objective function
-        self.model += lpSum(self.x[i][j][0] for i in self.I_set for j in self.K_set) * self.weight_factor + lpSum(self.t[i,j] * self.sim_mat[i][j] for i in self.I_set for j in self.I_set if j > i), "Objective"
+        self.model += lpSum(self.x[i][j][0] for i in self._I_set for j in self._K_set) * self.weight_factor + lpSum(self.t[i,j] * self.sim_mat[i][j] for i in self._I_set for j in self._I_set if j > i), "Objective"
 
         # Define base model constraints
         self.model.extend([ # Single Root Constraints
-            lpSum(self.x[i][k][0] for i in self.I_set) <= 1
-            for k in self.K_set
+            lpSum(self.x[i][k][0] for i in self._I_set) <= 1
+            for k in self._K_set
         ]) 
         self.model.extend([ # Threshold Constraints
-            lpSum(self.x[i][k][c] * self.spatial_attr[i] for i in self.I_set for c in self.C_set) >= self.threshold * lpSum(self.x[i][k][0] for i in self.I_set)
-            for k in self.K_set
+            lpSum(self.x[i][k][c] * self.spatial_attr[i] for i in self._I_set for c in self._C_set) >= self.threshold * lpSum(self.x[i][k][0] for i in self._I_set)
+            for k in self._K_set
         ])
         self.model.extend([ # Single Assignment Constraints
-            lpSum(self.x[i][k][c] for k in self.K_set for c in self.C_set) == 1
-            for i in self.I_set
+            lpSum(self.x[i][k][c] for k in self._K_set for c in self._C_set) == 1
+            for i in self._I_set
         ])
         self.model.extend([ # Adjacency Constraints
             self.x[i][k][c] <= lpSum(self.x[j][k][c-1] for j in self.spatial_weights.neighbors[i])
-            for i in self.I_set 
-            for k in self.K_set 
-            for c in self.C_set if c > 0
+            for i in self._I_set 
+            for k in self._K_set 
+            for c in self._C_set if c > 0
         ])
         self.model.extend([ # x-t Matching Constraints
-            self.t[i,j] <= lpSum(self.x[i][k][c] - self.x[j][k][c] for c in self.C_set) + 1
-            for i in self.I_set 
-            for j in self.I_set if j > i 
-            for k in self.K_set
+            self.t[i,j] <= lpSum(self.x[i][k][c] - self.x[j][k][c] for c in self._C_set) + 1
+            for i in self._I_set 
+            for j in self._I_set if j > i 
+            for k in self._K_set
         ])
 
     # solve MIP model
     def solve(self, time):
         self.model.solve(HiGHS(timeLimit=time, keepFiles=False))
-        self.maxp = int(value(lpSum(self.x[i][k][0] for i in self.I_set for k in self.K_set)))
+        self.maxp = int(value(lpSum(self.x[i][k][0] for i in self._I_set for k in self._K_set)))
         self.obj = value(self.model.objective)
 
-        assigned = {(i,k) for i in self.I_set for k in self.K_set for c in self.C_set if value(self.x[i][k][c]) > 0.9}
+        assigned = {(i,k) for i in self._I_set for k in self._K_set for c in self._C_set if value(self.x[i][k][c]) > 0.9}
         for i,k in assigned:
             self.regions[i] = k
 
